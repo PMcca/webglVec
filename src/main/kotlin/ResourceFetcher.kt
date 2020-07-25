42ad503b20ea40f85f6b32d46f5bd60020d9acc4
PMcca <petemcca@gmail.com>
2020-07-25 15:53:15 +0100

    Removes reflection, inits Resources via lambda

--- src/main/kotlin/ResourceFetcher.kt
+++ src/main/kotlin/ResourceFetcher.kt
@@ -2,7 +2,6 @@ import kotlin.browser.window
 import kotlin.js.Promise
 import kotlin.reflect.KMutableProperty0
 
-
 /**
  * Fetch resources via JavaScript's Fetch API
  */
@@ -22,33 +21,22 @@ class ResourceFetcher(
         val promises = mutableListOf<Promise<String>>()
 
         // Fetch shaders
-        promises.add(fetchResource(vertShaderUri, resources::vertSrc))
-        promises.add(fetchResource(fragShaderUri, resources::fragSrc))
-
-        //TODO: Use reflection above or code below?
-//        promises.add(window.fetch(vertShaderUri, params)
-//            .then {response -> response.text()}
-//            .then {text -> resources.vertSrc = text;text}
-//            .catch {e -> console.error("Fetch for $vertShaderUri failed with $e"); e.message!! })
-//        promises.add(window.fetch(fragShaderUri, params)
-//            .then {response -> response.text()}
-//            .then {text -> resources.fragSrc = text;text}
-//            .catch {e -> console.error("Fetch for $fragShaderUri failed with $e"); e.message!! })
-
+        promises.add(fetchResource(vertShaderUri) { s -> resources.vertSrc = s})
+        promises.add(fetchResource(fragShaderUri) {s -> resources.fragSrc = s})
 
         // Wait on all promises, then run app
         Promise.all(promises.toTypedArray()).then {_ -> run(resources)}
     }
 
-    private fun fetchResource(resUri: String, resPointer: KMutableProperty0<String>): Promise<String> {
+    private fun fetchResource(resUri: String, f: (String) -> Unit): Promise<String> {
         return window.fetch(resUri, params)
             .then {response -> response.text()}
-            .then {text -> resPointer.set(text);text}
+            .then {text -> f(text);text}
             .catch {e -> console.error("Fetch for $resUri failed with $e"); e.message!! }
     }
 }
 
-class Resources() {
+class Resources {
     lateinit var vertSrc: String
     lateinit var fragSrc: String
 }
